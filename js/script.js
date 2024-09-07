50352a7418f6fd797ffa1358b768fbf8e245e330
abhiiemail <50051726+abhiiemail@users.noreply.github.com>
2024-09-07 18:22:32 +0530

    Update script.js
    date adjusteed

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@
 //    console.log("Audio  ");
 //   }); 
 // Set the date we're counting down to
-var countDownDate = new Date("September 08, 2024 between 06:30:00").getTime();
+var countDownDate = new Date("September 08, 2024 06:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
